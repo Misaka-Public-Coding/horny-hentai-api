592f790abcb0bc4f3ad971d0c440113322710ed9
Misaka <misakachan2545@gmail.com>
2021-08-14 21:20:19 +0300

    routes?

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,25 @@
+const path = require("path");
+
+const assetsDir = path.join(__dirname,"/../storage/");
+
 const router = app => {
     app.get('/', (request, response) => {
         response.send({
-            message: 'HENTAI'
+            message: 'HENTAI API by естрёнка#8227'
+        });
+    });
+    app.get('/safe', (request, response) => {
+        let pathP = path.join(assetsDir,"safe/pic.png").toString();
+        response.sendFile(pathP,(e)=>{
+            console.log(e)
+        });
+    });
+    app.get('/unsafe', (request, response) => {
+        let pathP = path.join(assetsDir,"unsafe/pic.png").toString();
+        response.sendFile(pathP,(e)=>{
+            console.log(e)
         });
     });
 }
 
-// Export the router
 module.exports = router;
